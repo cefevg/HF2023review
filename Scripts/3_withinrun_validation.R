b96c418c4e797f1fac2d42299799099369cbc380
cefevg <99396261+cefevg@users.noreply.github.com>
2023-12-29 22:50:27 +0100

    plotting with and without outliers

--- Scripts/3_withinrun_validation.R
+++ Scripts/3_withinrun_validation.R
@@ -2,6 +2,18 @@ library(tidyverse)
 
 # Experimental validation review
 
+happy.data %>%
+  group_by(Inoculum, RUN_NAME, PLATE_NAME) %>%
+  summarise(mean.growth = mean(Surface), median.growth = median(Surface),
+            sd.growth = sd(Surface), mad.growth = mad(Surface),
+            outlier = ifelse(all(plate_meanoutlier=="Y"), "Mean_outlier",
+                             ifelse(all(plate_cvoutlier=="Y"), "CV_outlier", "N"))) %>%
+  mutate(cv.growth = sd.growth/mean.growth) %>%
+  ggplot(aes(x = as.factor(PLATE_NAME), y = mean.growth, col = as.factor(outlier))) +
+  geom_point() +
+  facet_wrap(~Inoculum, scales = "free") +
+  theme_bw()
+
 dmso.plates <- happy.data %>%
   filter(clean.data == "Y") %>%
   group_by(Inoculum, RUN_NAME, PLATE_NAME) %>%
