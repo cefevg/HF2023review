eb8d684efbcff54127c257bd186ffb41f9079d96
cefevg <99396261+cefevg@users.noreply.github.com>
2023-12-30 22:50:12 +0100

    Removed figures

--- Scripts/3_withinrun_validation.R
+++ Scripts/3_withinrun_validation.R
@@ -2,17 +2,6 @@ library(tidyverse)
 
 # Experimental validation review
 
-happy.data %>%
-  group_by(Inoculum, RUN_NAME, PLATE_NAME) %>%
-  summarise(mean.growth = mean(Surface), median.growth = median(Surface),
-            sd.growth = sd(Surface), mad.growth = mad(Surface),
-            outlier = ifelse(all(plate_meanoutlier=="Y"), "Mean_outlier",
-                             ifelse(all(plate_cvoutlier=="Y"), "CV_outlier", "N"))) %>%
-  mutate(cv.growth = sd.growth/mean.growth) %>%
-  ggplot(aes(x = as.factor(PLATE_NAME), y = mean.growth, col = as.factor(outlier))) +
-  geom_point() +
-  facet_wrap(~Inoculum, scales = "free") +
-  theme_bw()
 
 dmso.plates <- happy.data %>%
   filter(clean.data == "Y") %>%
@@ -20,15 +9,3 @@ dmso.plates <- happy.data %>%
   summarise(mean.growth = mean(Surface), median.growth = median(Surface),
             sd.growth = sd(Surface), mad.growth = mad(Surface)) %>%
   mutate(cv.growth = sd.growth/mean.growth)
-
-dmso.plates %>%
-  ggplot(aes(x = as.factor(PLATE_NAME), y = mean.growth)) +
-  geom_point() +
-  facet_wrap(~Inoculum, scales = "free") +
-  theme_bw()
-
-dmso.plates %>%
-  ggplot(aes(x = as.factor(PLATE_NAME), y = cv.growth)) +
-  geom_point() +
-  facet_wrap(~Inoculum, scales = "free") +
-  theme_bw()
