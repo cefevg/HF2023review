a004e864967808b6c8155c15967ff87a9d80e19d
cefevg <99396261+cefevg@users.noreply.github.com>
2023-12-30 22:52:00 +0100

    added 'conf intervals' and changed some defaults

--- Rshiny.R
+++ Rshiny.R
@@ -36,13 +36,13 @@ ui <- navbarPage("Experimental wondering",
       # Input: Decimal interval with step value ----
       numericInput("neg.cv", "DMSO surface CV:",
                   min = 0, max = 1,
-                  value = 0.5, step = 0.01),
+                  value = 0.15, step = 0.01),
       
       # Input: Animation with custom interval (in ms) ----
       # to control speed, plus looping
       numericInput("neg.reps", "DMSO replicates:",
                   min = 3, max = 100,
-                  value = 3, step = 1),
+                  value = 96, step = 1),
       
       # Input: Animation with custom interval (in ms) ----
       # to control speed, plus looping
@@ -92,7 +92,7 @@ tabPanel("CV - power analysis",
              # to control speed, plus looping
              numericInput("dmso.reps", "DMSO replicates:",
                           min = 3, max = 100,
-                          value = 3, step = 1),
+                          value = 96, step = 1),
              
              # Input: Animation with custom interval (in ms) ----
              # to control speed, plus looping
@@ -118,6 +118,47 @@ tabPanel("CV - power analysis",
              
            )
          )
+),
+
+# Within experiment vs among experiment variability
+
+tabPanel("Within vs among experiment variability",
+         
+         # Predictive stats title in the tab
+         
+         titlePanel("What's the effect of my variability?"),
+         
+         # Sidebar layout with input and output definitions ----
+         sidebarLayout(
+           
+           # Sidebar to demonstrate various slider options ----
+           sidebarPanel(
+             
+             # Input: Simple integer interval ----
+             numericInput("up.theshold", "Upper threshold:",
+                          min = 0, max = 1, value = 0.6, step = 0.01),
+             
+             # Input: Simple integer interval ----
+             numericInput("low.theshold", "Lower threshold:",
+                          min = 0, max = 1, value = 0.4, step = 0.01),
+             
+             # Input: Simple integer interval ----
+             numericInput("std.within", "Lower threshold:",
+                          min = 0, max = 1, value = 0.1, step = 0.01),
+             
+             
+             submitButton("Submit")
+             
+           ),
+           # Main panel for displaying outputs ----
+           mainPanel(
+             
+             
+             # Output: Table summarizing the values entered ----
+             plotOutput('cv.powerplot')
+             
+           )
+         )
 )
 
 )
@@ -139,34 +180,41 @@ server <- function(input, output) {
   
   simulations <- function(g1.mean, g1.std, g1.reps, g2.reps, alphalevel) {
     
-  Eff.tested <- seq(from = 0.50, to = 0.99, by = 0.01)
-  
-  results <- rep(0, length(Eff.tested))
-  
-  for (eff.sim in Eff.tested) {
+    results <- data.frame(Efficacy = seq(from = 0.50, to = 0.99, by = 0.01), 
+                          low.var = 0, 
+                          Power = 0,
+                          high.var = 0)
     
-    growth.red <- 1 - eff.sim
-    
-    signif <- rep(0, 1000)
-    
-    for (i in 1:1000) {
+    for (eff.sim in results$Efficacy) {
       
-      controls <- rnorm(g1.reps, g1.mean, g1.std)
+      growth.red <- 1 - eff.sim
       
-      molec <- rnorm(g2.reps, growth.red*g1.mean, g1.std)
+      signif <- data.frame(signif = rep(0, 1000), lowvar = rep(0, 1000), highvar = rep(0, 1000))
       
-      molec <- unlist(lapply(molec, function (x) 100*c(mean(controls)-x)/mean(controls)))
+      for (i in 1:1000) {
+        
+        controls <- rnorm(g1.reps, g1.mean, g1.std)
+        
+        molec <- rnorm(g2.reps, growth.red*g1.mean, g1.std)
+        molec.lowvar <- rnorm(g2.reps, growth.red*g1.mean, g1.std*0.5)
+        molec.highvar <- rnorm(g2.reps, growth.red*g1.mean, g1.std*1.25)
+        
+        molec <- unlist(lapply(molec, function (x) 100*c(mean(controls)-x)/mean(controls)))
+        molec.lowvar <- unlist(lapply(molec.lowvar, function (x) 100*c(mean(controls)-x)/mean(controls)))
+        molec.highvar <- unlist(lapply(molec.highvar, function (x) 100*c(mean(controls)-x)/mean(controls)))
+        
+        signif$signif[i] <- t.test(molec, mu = 50, alternative = "greater", conf.level = 1 - alphalevel/100)$p.value
+        signif$lowvar[i] <- t.test(molec.lowvar, mu = 50, alternative = "greater", conf.level = 1 - alphalevel/100)$p.value
+        signif$highvar[i] <- t.test(molec.highvar, mu = 50, alternative = "greater", conf.level = 1 - alphalevel/100)$p.value
+        
+      }
       
-      signif[i] <- t.test(molec, mu = 50, alternative = "greater", conf.level = 1 - alphalevel/100)$p.value
+      results[match(round(eff.sim,3), round(results$Efficacy, 3)),c(2:4)] <- c(sum(signif$lowvar<alphalevel/100)/10, sum(signif$signif<alphalevel/100)/10, sum(signif$highvar<alphalevel/100)/10)
       
     }
     
-    results[match(round(eff.sim,3), round(Eff.tested, 3))] <- sum(signif<0.1)/10
+    return(results)
     
-  }
-  
-  return(results)
-  
   }
   
   # Power as a function of CV
@@ -177,48 +225,53 @@ server <- function(input, output) {
     
     growth.red <- 1 - target.eff/100
     
-    target.sd <- 1
+    results <- data.frame(CV = seq(from = 0.01, to = 0.5, by = 0.01), 
+                          low.var = 0, 
+                          Power = 0,
+                          high.var = 0)
     
-    results <- rep(0, 50)
-    
-    cv.tested <- seq(from = 0.01, to = 0.5, by = 0.01)
-    
-    for (cv.sim in cv.tested) {
+    for (cv.sim in results$CV) {
       
       cv <- cv.sim
       
       std <- control.mean*cv
       
-      signif <- rep(0, 1000)
+      signif <- data.frame(lowvar = rep(0, 1000), signif = rep(0, 1000), highvar = rep(0, 1000))
       
       for (i in 1:1000) {
         
         controls <- rnorm(dmso.reps, control.mean, std)
         
         molec <- rnorm(target.reps, growth.red*control.mean, std)
+        molec.lowvar <-  rnorm(target.reps, growth.red*control.mean, std*0.5)
+        molec.highvar <-  rnorm(target.reps, growth.red*control.mean, std*1.25)
         
         molec <- unlist(lapply(molec, function (x) 100*c(mean(controls)-x)/mean(controls)))
+        molec.lowvar <- unlist(lapply(molec.lowvar, function (x) 100*c(mean(controls)-x)/mean(controls)))
+        molec.highvar <- unlist(lapply(molec.highvar, function (x) 100*c(mean(controls)-x)/mean(controls)))
         
-        signif[i] <- t.test(molec, mu = 50, alternative = "greater", conf.level = 1 - alphalevel2/100)$p.value
+        signif$signif[i] <- t.test(molec, mu = 50, alternative = "greater", conf.level = 1 - alphalevel2/100)$p.value
+        signif$lowvar[i] <- t.test(molec.lowvar, mu = 50, alternative = "greater", conf.level = 1 - alphalevel2/100)$p.value
+        signif$highvar[i] <- t.test(molec.highvar, mu = 50, alternative = "greater", conf.level = 1 - alphalevel2/100)$p.value
         
       }
       
-      results[match(round(cv.sim,3), round(cv.tested, 3))] <- sum(signif<0.1)/10
+      results[match(round(cv.sim,3), round(results$CV, 3)),c(2:4)] <- c(sum(signif$lowvar<alphalevel2/100)/10, sum(signif$signif<alphalevel2/100)/10, sum(signif$highvar<alphalevel2/100)/10)
       
     }
     
-    res.rep <- data.frame(CV = cv.tested, Power = results)
-    
-    return(res.rep)
+    return(results)
     
   }
   
   # Show the values in an HTML table ----
   output$powerplot <- renderPlot({
     
-      ggplot(data.frame(Efficacy = seq(from = 0.50, to = 0.99, by = 0.01), Power = simulations(input$neg.mean,input$neg.cv, input$neg.reps, input$comp.reps, input$alpha)),
+      ggplot(simulations(input$neg.mean,input$neg.cv, input$neg.reps, input$comp.reps, input$alpha),
              aes(x = Efficacy, y = Power)) +
       geom_line() +
+      geom_line(aes(y = low.var), linetype="dotted") +
+      geom_line(aes(y = high.var), linetype = "dotted") +
       scale_x_continuous(breaks = seq(from = 0.50, to = 0.99, by = 0.01)) +
       geom_hline(yintercept = 80, col = "red") +
       theme_bw() +
@@ -231,6 +284,8 @@ server <- function(input, output) {
     
       ggplot(cvsimulations(input$target.eff, input$target.reps, input$dmso.reps, input$alpha.sec), aes(x = CV, y = Power)) +
       geom_line() +
+      geom_line(aes(y = low.var), linetype="dotted") +
+      geom_line(aes(y = high.var), linetype = "dotted") +
       scale_x_continuous(breaks = seq(from = 0.01, to = 0.5, by = 0.01)) +
       geom_hline(yintercept = 80, col = "red") +
       theme_bw() +
