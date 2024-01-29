c3854ca3c9c039e3b058f4683a4557d30c7cb49d
cefevg <99396261+cefevg@users.noreply.github.com>
2024-01-29 08:59:31 +0100

    Update 0_visualizations.R

--- Scripts/0_visualizations.R
+++ Scripts/0_visualizations.R
@@ -2,6 +2,59 @@
 
 ## DMSO
 
+# Well outliers example
+
+outlier.data %>%
+  filter(Inoculum == "BOTRCI" & RUN_NAME == "IM230703-3-CONF") %>%
+  ggplot(aes(x = as.factor(PLATE_NAME), y = Surface, col = Well_outlier)) +
+  geom_point() +
+  scale_y_continuous(limits = c(0, 0.5)) +
+  theme_bw()
+
+outlier_detection(dmso.data, dmso.plates, 7) %>%
+  ggplot(aes(x = Well_outlier)) +
+  geom_bar()
+
+table(outlier_detection(dmso.data, dmso.plates, 7)$Well_outlier)
+
+outlier_detection(dmso.data, dmso.plates, 10) %>%
+  group_by(Inoculum) %>%
+  summarise(ratio = sum(Well_outlier=="Y")/n())
+
+# Plate outliers
+
+outlier.data %>%
+  filter(Inoculum == "BOTRCI" & RUN_NAME == "IM230703-3-CONF" & Well_outlier == "N") %>%
+  ggplot(aes(x = as.factor(PLATE_NAME), y = Surface)) +
+  geom_boxplot() +
+  geom_hline(yintercept = run.data$acceptrun.highthres[run.data$RUN_NAME=="IM230703-3-CONF" & run.data$Inoculum=="BOTRCI"], linetype = "dashed") +
+  geom_hline(yintercept = run.data$acceptrun.lowthres[run.data$RUN_NAME=="IM230703-3-CONF" & run.data$Inoculum=="BOTRCI"], linetype = "dashed") +
+  geom_hline(yintercept = run.data$mean.run[run.data$RUN_NAME=="IM230703-3-CONF" & run.data$Inoculum=="BOTRCI"]) +
+  scale_y_continuous(limits = c(0.15, 0.45)) +
+  theme_bw()
+
+out.plates %>%
+  filter(Inoculum == "FUSACU" & RUN_NAME == "IM221024-7-ENT") %>%
+  ggplot(aes(x = as.factor(PLATE_NAME), y = mean.clean)) +
+  geom_point() +
+  geom_errorbar(aes(ymin = plate.lowconf, ymax = plate.upconf)) +
+  geom_hline(yintercept = run.data$acceptrun.highthres[run.data$RUN_NAME=="IM221024-7-ENT" & run.data$Inoculum=="FUSACU"], linetype = "dashed") +
+  geom_hline(yintercept = run.data$acceptrun.lowthres[run.data$RUN_NAME=="IM221024-7-ENT" & run.data$Inoculum=="FUSACU"], linetype = "dashed") +
+  geom_hline(yintercept = run.data$mean.run[run.data$RUN_NAME=="IM221024-7-ENT" & run.data$Inoculum=="FUSACU"]) +
+  geom_hline(yintercept = run.data$cv.highthres[run.data$RUN_NAME=="IM221024-7-ENT" & run.data$Inoculum=="FUSACU"], linetype = "3313") +
+  geom_hline(yintercept = run.data$cv.lowthres[run.data$RUN_NAME=="IM221024-7-ENT" & run.data$Inoculum=="FUSACU"], linetype = "3313") +
+  theme_bw()
+
+out.plates %>%
+  filter(Inoculum == "FUSACU" & RUN_NAME == "IM230717-2-ENT") %>%
+  ggplot(aes(x = as.factor(PLATE_NAME), y = plate.cv)) +
+  geom_point() +
+  geom_hline(yintercept = 0.2) +
+  theme_bw()
+
+table(out.plates$plate_meanoutlier)
+
+
 # Mean per plate and cv per plate with no well-outliers, then tagging plate-outliers
 
 outlier.data %>%
@@ -99,4 +152,49 @@ refs.plates %>%
   ggplot(aes(x = as.factor(PLATE_NAME), y = cv.growth, col = as.factor(DOSE))) +
   geom_point() +
   facet_wrap(~SAMPLE_NAME, scales = "free") +
+  theme_bw()
+
+
+## Standards
+
+# Well outliers example
+
+outlier.refsdata %>%
+  filter(Inoculum == "BOTRCI" & RUN_NAME == "IM230703-3-CONF" & SAMPLE_NAME == "Cyprodinil" & DOSE == 2) %>%
+  ggplot(aes(x = as.factor(PLATE_NAME), y = Surface, col = Well_refsoutlier)) +
+  geom_point() +
+  scale_y_continuous(limits = c(0, 0.5)) +
+  theme_bw() 
+
+outlier_detection(refs.data,refs.plates, 7) %>%
+  ggplot(aes(x = Well_refsoutlier)) +
+  geom_bar()
+
+# Plate outliers
+
+outlier.refsdata %>%
+  filter(Inoculum == "BOTRCI" & RUN_NAME == "IM230703-3-CONF" & Well_refsoutlier == "N" & SAMPLE_NAME == "Cyprodinil" & DOSE == 2) %>%
+  ggplot(aes(x = as.factor(PLATE_NAME), y = Surface)) +
+  geom_boxplot() +
+  scale_y_continuous(limits = c(0, 0.45)) +
+  facet_wrap(~SAMPLE_NAME*DOSE) +
+  theme_bw()
+
+outlier.refsdata %>%
+  filter(Inoculum == "BOTRCI" & RUN_NAME == "IM230703-3-CONF" & Well_refsoutlier == "N" & SAMPLE_NAME == "HAMBRA" & DOSE == 1.25) %>%
+  ggplot(aes(x = as.factor(PLATE_NAME), y = Surface)) +
+  geom_boxplot() +
+  geom_hline(yintercept = run.refsdata$acceptrun.highthres[run.refsdata$RUN_NAME=="IM230703-3-CONF" & run.refsdata$Inoculum=="BOTRCI" & run.refsdata$SAMPLE_NAME == "HAMBRA" & run.refsdata$DOSE == 1.25], linetype = "dashed") +
+  geom_hline(yintercept = run.refsdata$acceptrun.lowthres[run.refsdata$RUN_NAME=="IM230703-3-CONF" & run.refsdata$Inoculum=="BOTRCI" & run.refsdata$SAMPLE_NAME == "HAMBRA" & run.refsdata$DOSE == 1.25], linetype = "dashed") +
+  geom_hline(yintercept = run.refsdata$mean.run[run.refsdata$RUN_NAME=="IM230703-3-CONF" & run.refsdata$Inoculum=="BOTRCI" & run.refsdata$SAMPLE_NAME == "HAMBRA" & run.refsdata$DOSE == 1.25]) +
+  scale_y_continuous(limits = c(0, 0.45)) +
+  theme_bw()
+
+
+out.refsplates %>%
+  filter(Inoculum == "FUSACU" & RUN_NAME == "IM230717-2-ENT") %>%
+  ggplot(aes(x = as.factor(PLATE_NAME), y = platerefs.cv)) +
+  geom_point() +
+  geom_hline(yintercept = 0.2) +
+  facet_wrap(~SAMPLE_NAME*DOSE) +
   theme_bw()
