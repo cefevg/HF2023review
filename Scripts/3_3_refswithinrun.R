55ee228436078915afa36a4b4ca7daa07ba6e1ca
cefevg <99396261+cefevg@users.noreply.github.com>
2024-01-29 09:01:38 +0100

    Update 3_3_refswithinrun.R

--- Scripts/3_3_refswithinrun.R
+++ Scripts/3_3_refswithinrun.R
@@ -8,23 +8,24 @@ library(tidyverse)
 run.refsdata <- refs.data %>%
   group_by(Inoculum, RUN_NAME, SAMPLE_NAME, DOSE) %>%
   summarise(mean.run = mean(Surface), median.run = median(Surface),
-            sd.run = sd(Surface), mad.run = mad(Surface)) %>%
+            sd.run = sd(Surface), mad.run = mad(Surface), cv.run = sd.run/mean.run) %>%
   mutate(acceptrun.lowthres = mean.run-sd.run,
-         acceptrun.highthres = mean.run+sd.run)
+         acceptrun.highthres = mean.run+sd.run,
+         cv.highthres = mean.run+cv.run/5,
+         cv.lowthres = mean.run-cv.run/5)
 
 plate_refsoutlier <- function (outlier.refsdata, run.refsdata) {
   
   clean_data <- outlier.refsdata %>%
-    filter(RefsOutlier == "N") %>%
+    filter(Well_refsoutlier == "N") %>%
     group_by(Inoculum, RUN_NAME, PLATE_NAME, SAMPLE_NAME, DOSE) %>%
     summarise(sd.clean = sd(Surface),
               mean.clean = mean(Surface),
               platerefs.upconf = ifelse(sd.clean == 0, mean(Surface), t.test(Surface)$conf.int[2]), 
               platerefs.lowconf = ifelse(sd.clean == 0, mean(Surface), t.test(Surface)$conf.int[1]),
               platerefs.cv = sd.clean/mean.clean) %>%
-    select(!c(sd.clean, mean.clean)) %>%
     left_join(run.refsdata) %>%
-    mutate(platerefs_meanoutlier = ifelse(platerefs.upconf > acceptrun.highthres | platerefs.lowconf < acceptrun.lowthres, "Y", "N"),
+    mutate(platerefs_meanoutlier = ifelse(platerefs.upconf < acceptrun.lowthres | platerefs.lowconf > acceptrun.highthres, "Y", "N"),
            platerefs_cvoutlier = ifelse(platerefs.cv > 0.2, "Y", "N"))
   
   return(clean_data)
@@ -34,8 +35,9 @@ plate_refsoutlier <- function (outlier.refsdata, run.refsdata) {
 out.refsplates <- plate_refsoutlier(outlier.refsdata, run.refsdata) 
 
 happy.refs <- out.refsplates %>%
+  select(!c(sd.clean, mean.clean)) %>%
   right_join(outlier.refsdata) %>%
-  mutate(clean.refs = ifelse(platerefs_meanoutlier=="Y" | RefsOutlier == "Y" | platerefs_cvoutlier =="Y", "N", "Y"))
+  mutate(clean.refs = ifelse(platerefs_meanoutlier=="Y" | Well_refsoutlier == "Y" | platerefs_cvoutlier =="Y", "N", "Y"))
 
 refs.plates <- happy.refs %>%
   filter(clean.refs == "Y") %>%
